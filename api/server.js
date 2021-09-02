392fafa940c4dfc0d071f9644c2700a2d07deb29
Hamida Sh <sharifovahamida@gmail.com>
2021-09-01 22:57:13 -0700

    Initial changes

--- api/server.js
+++ api/server.js
@@ -1,3 +1,20 @@
 // BUILD YOUR SERVER HERE
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+ // EXPORT YOUR SERVER instead of {}
+
+const express = require('express');
+
+const server = express();
+
+server.use(express.json())
+
+server.post('/api/users', (req, res) => {
+   
+
+    
+})
+
+
+
+
+module.exports = server;
--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const server = require('./api/server');
 const port = 5000;
 
 // START YOUR SERVER HERE
+
+
+
+
+server.listen(port, () => {
+    console.log(`port is running on ${port}`)
+})
+
+module.exports = server;
